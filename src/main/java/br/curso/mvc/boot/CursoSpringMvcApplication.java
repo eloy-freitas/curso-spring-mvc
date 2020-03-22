2ca61fb268ce6af4d9d08635b5ff4f98522d3bfa
eloy <freitaseloy.1@gmail.com>
2020-03-22 12:16:06 -0300

    v1.0 - add dependencias, add arquivos de front-end do projeto e mapeando os metodos de exibiçao das paginas web

---
+++ src/main/java/br/curso/mvc/boot/CursoSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package br.curso.mvc.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/curso/mvc/boot/CursoSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.curso.mvc.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
