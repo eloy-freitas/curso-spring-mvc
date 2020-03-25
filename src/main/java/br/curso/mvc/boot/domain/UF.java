e8c4ae2d0e297f945a74b49256a4803401459d1d
eloy <freitaseloy.1@gmail.com>
2020-03-25 11:12:34 -0300

    adição das classes de dominio e mapeamentos das com o Hibernate

---
+++ src/main/java/br/curso/mvc/boot/domain/UF.java
@@ -0,0 +1,51 @@
+package br.curso.mvc.boot.domain;
+
+public enum UF {
+	AC("AC", "Acre"), 
+	AL("AL", "Alagoas"),
+	AM("AM", "Amazonas"),
+	BA("BA", "Bahia"),
+	CE("CE", "Ceará"),
+	DF("DF", "Distrito Federal"),
+	ES("ES", "EspÃ­rito Santo"),
+	GO("GO", "Goiás"),
+	MA("MA", "Maranhão"),
+	MT("MT", "Mato Grosso"),
+	MS("MS", "Mato Grosso do Sul"),
+	MG("MG", "Minas Gerais"),
+	PA("PA", "Pará"),
+	PB("PB", "Paraí­ba"),
+	PR("PR", "Paraná"),
+	PE("PE", "Pernambuco"),
+	PI("PI", "Piauí­"),
+	RJ("RJ", "Rio de Janeiro"),
+	RN("RN", "Rio Grande do Norte"),
+	RS("RS", "Rio Grande do Sul"),
+	RO("RO", "Rondônia"),
+	RR("RR", "Roraima"),
+	SC("SC", "Santa Catarina"),
+	SP("SP", "São Paulo"),
+	SE("SE", "Sergipe"),
+	TO("TO", "Tocantins");
+	
+	private String sigla;
+	private String descricao;
+	private UF(String sigla, String descricao) {
+		this.sigla = sigla;
+		this.descricao = descricao;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
